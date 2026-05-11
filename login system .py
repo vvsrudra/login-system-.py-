926ba49636bccfa07fcd61216170b33aaf2ad082
VVS RUDRA <vvsrudra@gmail.com>
2026-05-11 21:20:57 +0530

    Add files via upload

---
+++ login system .py
@@ -0,0 +1,7 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "Rudra" and password == "python123":
+    print("Login successful")
+else:
+    print("Invalid credentials")
